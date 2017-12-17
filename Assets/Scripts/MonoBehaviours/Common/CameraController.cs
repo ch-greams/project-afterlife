be7bbd3d52ce84706e0ee4359fd1c210f509bf82
Andrey <andrey.greams@gmail.com>
2017-12-17 09:06:47 +0300

    Small restructure

--- Assets/Scripts/MonoBehaviours/Common/CameraController.cs
+++ Assets/Scripts/MonoBehaviours/Common/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Rendering;
 
 
-public class CameraControl : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     public bool rotateEnabled = true;
     public float rotateSpeed = 400F;
