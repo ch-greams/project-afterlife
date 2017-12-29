38e98f12794de8b5ac438ae1b5bedcab462113f9
Andrey <andrey.greams@gmail.com>
2017-12-29 10:22:22 +0300

    Update SceneReaction with default value for DoorInteractable

--- Assets/Scripts/Actions/Reactions/SceneReaction.cs
+++ Assets/Scripts/Actions/Reactions/SceneReaction.cs
@@ -1,25 +1,56 @@
 ﻿using System.Collections;
+using Sirenix.OdinInspector;
 
 
 public class SceneReaction : IReaction
 {
+    public SceneReactionType type;
+    [ShowIf("type", SceneReactionType.DEFAULT)]
     public SceneType scene;
+    [ShowIf("type", SceneReactionType.DEFAULT)]
     public Point startPoint;
 
     private GlobalController globalCtrl;
+    private SceneType defaultScene;
+    private Point defaultPosition;
 
 
     public void Init(Interactable interactable)
     {
         this.globalCtrl = interactable.sceneCtrl.globalCtrl;
+
+        switch (interactable.GetType().Name)
+        {
+            case "DoorInteractable":
+                DoorInteractable di = interactable as DoorInteractable;
+                this.defaultScene = di.door.toScene;
+                this.defaultPosition = di.door.exitPoint;
+                break;
+            default:
+                break;
+        }
     }
     public IEnumerator React()
     {
-        // Update start point in state
-        this.globalCtrl.UpdatePlayerPosition(this.scene, this.startPoint);
-        // Switch scene
-        SceneManager.FadeAndLoadScene(this.scene.ToString());
-
-        yield return null;
+        switch (this.type)
+        {
+            case SceneReactionType.DOOR_TRIGGER:
+                this.globalCtrl.UpdatePlayerPosition(this.defaultScene, this.defaultPosition);
+                SceneManager.FadeAndLoadScene(this.defaultScene.ToString());
+                break;
+            case SceneReactionType.DEFAULT:
+                this.globalCtrl.UpdatePlayerPosition(this.scene, this.startPoint);
+                SceneManager.FadeAndLoadScene(this.scene.ToString());
+                break;
+            default:
+                yield return null;
+                break;
+        }
     }
+}
+
+public enum SceneReactionType
+{
+    DEFAULT,
+    DOOR_TRIGGER,
 }
